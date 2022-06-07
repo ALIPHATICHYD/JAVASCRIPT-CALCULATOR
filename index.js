dc3916e32ad8d0dc5046f8b29c14a0aaa510012b
Adegoke Fuad Ajibola <105937740+ALIPHATICHYD@users.noreply.github.com>
2022-06-08 00:43:10 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 
+function getHistory(){
+   return document.getElementsByClassName("history_value").innertText;
+
+}
+
+function printHistory(num){
+    document.getElementsByClassName("history_value").innertText=num
+}
+
+printHistory("9*9")
